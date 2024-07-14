22e08657d0621b357db4585d140836fba3fdefdd
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2024-07-14 22:16:57 +0600

    initial setup

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,16 @@
+type TResdata<T> = {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data: T;
+};
+
+const sendResponse = <T>(res: Response, data: TResdata<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    message: data.message,
+    data: data.data,
+  });
+};
+
+export default sendResponse;
